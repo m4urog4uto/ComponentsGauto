fdafd5ea63cd269ca50d621983b152863c49e1db
Mauro Gauto <mauro.gauto@globallogic.com>
2023-03-17 23:11:57 -0300

    components practice finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { LeftMenuComponent } from './components/left-menu/left-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    LayoutComponent,
+    LeftMenuComponent
   ],
   imports: [
     BrowserModule
